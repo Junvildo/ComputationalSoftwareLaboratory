ffcb78d17ae82d148287291e3f33e91941855b81
Tran Nguyen Tien Loc <manasuma@localhost.localdomain>
2022-10-26 10:19:08 +0700

    Done 1-5

--- Week7/classwork/Bai3.m
+++ Week7/classwork/Bai3.m
@@ -5,4 +5,4 @@ x=-5:.2:5;
 y=x./(1+x.^4);
 plot(x,y,'-. red','LineWidth',2);
 hold on
-plot(x,y,'o blue',6);
+plot(x,y,' b o','LineWidth',6);
--- Week7/classwork/Bai5.m
+++ Week7/classwork/Bai5.m
@@ -1,10 +1,22 @@
 clc
 clear all
 close all
-x=-10:.1:10;
+x=-10:1:10;
 y=x.^3-3*x;
+f1=3*x.^2-3;
+% Cau a
+figure
 plot(x,y);
+for i=1:21
+    if f1(i)==0
+        text(i-11,y(i),'Cuc Tri');
+    end
+end
 title('y=x^{3}-3x');
-f=3*x.^2-3;
+% Cau b
+f2=6*x;
+figure
+plot(x,f1);
 hold on
-plot(x,f);
+plot(x,f2);
+legend("f'1=3x^{2}-3","f'2=6x");
